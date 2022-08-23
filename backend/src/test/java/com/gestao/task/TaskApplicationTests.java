167b4fc850623744ab018698c20da6cdc893ae62
Jordan <jordanrafael92@gmail.com>
2022-08-23 17:48:14 -0300

    Created Project

---
+++ backend/src/test/java/com/gestao/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
